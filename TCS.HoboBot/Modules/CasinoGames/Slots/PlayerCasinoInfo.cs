daf0d8f5e0d2a5aaf7050445480ee475681d155b
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-05-23 11:55:00 -0700

    Create PlayerCasinoInfo.cs

---
+++ TCS.HoboBot/Modules/CasinoGames/Slots/PlayerCasinoInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+namespace TCS.HoboBot.Modules.CasinoGames.Slots;
+
+public class PlayerCasinoInfo {
+    public static ConcurrentDictionary<ulong, Dictionary<ulong, PlayerCasinoInfo>> Cache = new();
+    //public static readonly ConcurrentDictionary<ulong, PlayerCasinoInfo> PlayerCasinoInfo = new();
+    public ulong GuildId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+
+    public float TotalBetAmount { get; set; }
+    public float TotalWinAmount { get; set; }
+    public float TotalLostAmount { get; set; }
+
+    public int SpinCount { get; set; }
+    public int TotalSpinWins { get; set; }
+    public int TotalSpinLosses { get; set; }
+    public int HighestSpinWinStreak { get; set; }
+}
