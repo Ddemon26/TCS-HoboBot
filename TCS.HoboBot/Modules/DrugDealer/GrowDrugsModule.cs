849edcb9b06ed35563176c711e5b5670e7093b19
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-05-23 11:54:56 -0700

    Update GrowDrugsModule.cs

--- TCS.HoboBot/Modules/DrugDealer/GrowDrugsModule.cs
+++ TCS.HoboBot/Modules/DrugDealer/GrowDrugsModule.cs
@@ -4,13 +4,13 @@ using TCS.HoboBot.Modules.Moderation;
 namespace TCS.HoboBot.Modules.DrugDealer;
 
 public class CookDrugsModule : InteractionModuleBase<SocketInteractionContext> {
-    public enum CookType { Cocaine, Heroin, Crack, Meth, Lsd, Ecstasy, }
+    public enum CookType { Cocaine, Heroin, Crack, Meth, Lsd, Ecstasy }
     static readonly Random Rng = new();
 
     [SlashCommand( "cook", "Cook some drugs" )]
     public async Task CookAsync(CookType type) {
         ulong userId = Context.User.Id;
-        var stash = PlayersStashes.GetStash( userId );
+        var stash = PlayersStashes.GetStash(Context.Guild.Id,  userId );
 
         var requiredRole = type switch {
             CookType.Cocaine => DealerRole.PettyDrugDealer,
@@ -19,7 +19,7 @@ public class CookDrugsModule : InteractionModuleBase<SocketInteractionContext> {
             CookType.Meth => DealerRole.Kingpin,
             CookType.Lsd => DealerRole.DrugLord,
             CookType.Ecstasy => DealerRole.Underboss,
-            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null )
+            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null ),
         };
 
         if ( stash.Role < requiredRole ) {
@@ -38,7 +38,7 @@ public class CookDrugsModule : InteractionModuleBase<SocketInteractionContext> {
             CookType.Meth => (PlayersStashes.NextMethCook, PlayersStashes.CookCooldown),
             CookType.Lsd => (PlayersStashes.NextLsdCook, PlayersStashes.CookCooldown),
             CookType.Ecstasy => (PlayersStashes.NextEcstasyCook, PlayersStashes.CookCooldown),
-            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null )
+            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null ),
         };
 
         if ( nextDict.TryGetValue( userId, out var nextCook ) && now < nextCook ) {
@@ -59,10 +59,10 @@ public class CookDrugsModule : InteractionModuleBase<SocketInteractionContext> {
             CookType.Meth => DrugType.Meth,
             CookType.Lsd => DrugType.Lsd,
             CookType.Ecstasy => DrugType.Ecstasy,
-            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null )
+            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null ),
         };
         stash.AddAmountToType( drug, amount );
-        PlayersStashes.Stash[userId] = stash;
+        PlayersStashes.SaveStash( Context.Guild.Id, userId, stash );
 
         await RespondAsync( $"You cooked {amount}g of {type.ToString().ToLower()}!" );
     }
@@ -75,13 +75,13 @@ public class GrowDrugsModule : InteractionModuleBase<SocketInteractionContext> {
     [SlashCommand( "grow", "Grow some weed or shrooms" )]
     public async Task GrowAsync(GrowType type) {
         ulong userId = Context.User.Id;
-        var stash = PlayersStashes.GetStash( userId );
+        var stash = PlayersStashes.GetStash( Context.Guild.Id, userId );
 
         var requiredRole = type switch {
             GrowType.Weed => DealerRole.LowLevelDealer,
             GrowType.Shrooms => DealerRole.PettyDrugDealer,
             GrowType.Dmt => DealerRole.Godfather,
-            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null )
+            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null ),
         };
 
         if ( stash.Role < requiredRole ) {
@@ -96,7 +96,7 @@ public class GrowDrugsModule : InteractionModuleBase<SocketInteractionContext> {
         (ConcurrentDictionary<ulong, DateTimeOffset> nextDict, var cooldown) = type switch {
             GrowType.Weed => (PlayersStashes.NextWeedGrow, PlayersStashes.GrowCooldown),
             GrowType.Shrooms => (PlayersStashes.NextShroomGrow, PlayersStashes.GrowCooldown),
-            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null )
+            _ => throw new ArgumentOutOfRangeException( nameof(type), type, null ),
         };
 
         if ( nextDict.TryGetValue( userId, out var nextGrow ) && now < nextGrow ) {
@@ -112,7 +112,7 @@ public class GrowDrugsModule : InteractionModuleBase<SocketInteractionContext> {
         int amount = Rng.Next( 5, 26 );
         var drug = type == GrowType.Weed ? DrugType.Weed : DrugType.Shrooms;
         stash.AddAmountToType( drug, amount );
-        PlayersStashes.Stash[userId] = stash;
+        PlayersStashes.SaveStash( Context.Guild.Id, userId, stash );
 
         await RespondAsync( $"You grew {amount}g of {type.ToString().ToLower()}!" );
     }
