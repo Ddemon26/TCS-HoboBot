39bed6cb54ccf1e70578521d9d17c629c79c2318
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-05-23 11:54:57 -0700

    Update CheckStashModule.cs

--- TCS.HoboBot/Modules/DrugDealer/CheckStashModule.cs
+++ TCS.HoboBot/Modules/DrugDealer/CheckStashModule.cs
@@ -4,7 +4,7 @@ namespace TCS.HoboBot.Modules.DrugDealer;
 public class CheckStashModule : InteractionModuleBase<SocketInteractionContext> {
     [SlashCommand( "check_stash", "Check your stash" )]
     public async Task CheckStashAsync() {
-        var stash = PlayersStashes.GetStash( Context.User.Id );
+        var stash = PlayersStashes.GetStash( Context.Guild.Id, Context.User.Id );
         await RespondAsync( $"Your stash:\n{stash.GetDrugsString()}" );
     }
 }
