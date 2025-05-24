f4f076c0a63fd9f797cfea05daeb1a34044a4278
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-05-24 04:29:09 -0700

    Create CheckJackpotsModule.cs

---
+++ TCS.HoboBot/Data/CheckJackpotsModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+namespace TCS.HoboBot.Modules.CasinoGames.Slots;
+
+public class CheckJackpotsModule : InteractionModuleBase<SocketInteractionContext> {
+    [SlashCommand( "jackpots_check", "Check the current jackpots for this guild")]
+    public async Task CheckJackPotsAsync() {
+        if ( CasinoManager.JackPotsCache.TryGetValue( Context.Guild.Id, out var jackpots ) ) {
+            await RespondAsync( jackpots.ToString() );
+        }
+        else {
+            await RespondAsync( "No jackpots found for this guild." );
+        }
+    }
+}
