bc606747d278c364af338e74ceae8b13f82598b4
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-05-25 15:23:41 -0700

    Create DiscordGuildMemberCache.cs

---
+++ TCS.HoboBot/Data/DiscordGuildMemberCache.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+using Discord.Rest;
+namespace TCS.HoboBot.Modules.CasinoGames.Slots;
+
+public static class DiscordGuildMemberCache {
+    public static readonly ConcurrentDictionary<ulong, RestGuildUser[]> GuildMembers = new();
+
+    public static void AddMembers(ulong guildId, RestGuildUser[] members) => GuildMembers[guildId] = members;
+
+    public static RestGuildUser? GetUser(ulong guildId, ulong userId) =>
+        GuildMembers.TryGetValue( guildId, out RestGuildUser?[]? members )
+            ? members.OfType<RestGuildUser>().FirstOrDefault( m => m.Id == userId )
+            : null;
+}
