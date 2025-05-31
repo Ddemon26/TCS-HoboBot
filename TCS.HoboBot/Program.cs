812976b0b59fe412e7a738749ce71f17cf04f72a
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-05-31 04:38:49 -0700

    Update Program.cs

--- TCS.HoboBot/Program.cs
+++ TCS.HoboBot/Program.cs
@@ -1,56 +1,52 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using Discord.Interactions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using TCS.HoboBot;
-using TCS.HoboBot.YoutubeMusic;
+using TCS.HoboBot.Services;
 
-var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) =>
-    {
-        services.Configure<HostOptions>(opt => opt.ShutdownTimeout = TimeSpan.FromSeconds(30));
+var host = Host.CreateDefaultBuilder( args )
+    .ConfigureAppConfiguration( (context, config) => {
+            config.AddUserSecrets<Program>();
+        }
+    )
+    .ConfigureServices( (context, services) => {
+            services.Configure<HostOptions>( opt => opt.ShutdownTimeout = TimeSpan.FromSeconds( 30 ) );
 
-        var discordConfig = new DiscordSocketConfig
-        {
-            GatewayIntents = GatewayIntents.Guilds
-                             | GatewayIntents.GuildMessages
-                             | GatewayIntents.MessageContent
-                             | GatewayIntents.GuildIntegrations 
-                             | GatewayIntents.GuildMembers 
-                             | GatewayIntents.GuildVoiceStates,
-        };
-        services.AddSingleton(discordConfig);
-        services.AddSingleton<DiscordSocketClient>();
+            var discordConfig = new DiscordSocketConfig {
+                GatewayIntents = GatewayIntents.Guilds
+                                 | GatewayIntents.GuildMessages
+                                 | GatewayIntents.MessageContent
+                                 | GatewayIntents.GuildIntegrations
+                                 | GatewayIntents.GuildMembers
+                                 | GatewayIntents.GuildVoiceStates,
+            };
 
-        var interactionConfig = new InteractionServiceConfig
-        {
-            LogLevel = LogSeverity.Info,
-            UseCompiledLambda = true,
-            DefaultRunMode = RunMode.Async,
-        };
-        services.AddSingleton(sp =>
-            new InteractionService(sp.GetRequiredService<DiscordSocketClient>(), interactionConfig));
-        
-        // My Custom Entry Points
+            services.AddSingleton( discordConfig );
+            services.AddSingleton<DiscordSocketClient>();
 
-        services.AddHostedService<BotService>();
-        
-        services.AddHostedService<MessageResponder>();
-        
-        services.AddHostedService<RoleService>();
-        
-        //services.AddHostedService<MusicService>();
-    })
+            var interactionConfig = new InteractionServiceConfig {
+                LogLevel = LogSeverity.Info,
+                UseCompiledLambda = true,
+                DefaultRunMode = RunMode.Async,
+            };
+
+            services.AddSingleton
+            ( sp => new InteractionService
+              (
+                  sp.GetRequiredService<DiscordSocketClient>(),
+                  interactionConfig
+              )
+            );
+
+            // My Custom Entry Points
+            services.AddHostedService<BotService>();
+            services.AddHostedService<MessageResponderService>();
+            services.AddHostedService<RoleService>();
+        }
+    )
     .UseConsoleLifetime() // registers Ctrl+C/SIGTERM handlers
     .Build();
-
-// Hook into the host’s stopping event
-// var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
-// lifetime.ApplicationStopping.Register(() =>
-// {
-//     var bot = host.Services.GetRequiredService<BotService>();
-//     bot.SaveDataOnShutdown().GetAwaiter().GetResult();
-// });
 
 await host.RunAsync();
